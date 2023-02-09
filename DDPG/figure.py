cc7574e87ab0c8fa9029763b04f7f3d15953adb6
js <dldnxks12@naver.com>
2023-02-09 17:54:18 +0900

    DDPG check

---
+++ DDPG/figure.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+
+#for _ in range(10):
+
+# Add noise to Action
+class OrnsteinUhlenbeckNoise:
+    def __init__(self, mu):
+        self.theta, self.dt, self.sigma = 0.1, 0.01, 0.1
+        self.mu = mu
+        self.x_prev = np.zeros_like(self.mu)
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+
+a = np.zeros(1)
+for _ in range(10):
+    noise = OrnsteinUhlenbeckNoise(np.zeros(1))
+    print(noise())
